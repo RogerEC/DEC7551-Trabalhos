45d8e113e51c6d2262d197128c5315366ef602b7
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-12-18 16:34:29 -0300

    Conclusão da implementação

--- trabalho-02/cliente/www/js/index.js
+++ trabalho-02/cliente/www/js/index.js
@@ -1,24 +1,8 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+const HOST_IP = "https://192.168.0.106:3000";
+var tokenAcesso = "";
+var user = "";
+var request;
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
@@ -26,4 +10,255 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     //document.getElementById('deviceready').classList.add('ready');
+    
+    $("#BOTAO-ENTRAR").on("click", function(event){
+        event.preventDefault();
+        if(validarDadosLogin() == false){
+            return;
+        }
+        if (request) {
+            request.abort();
+        }
+        request = $.ajax({
+            type: "POST",
+            url: HOST_IP+"/login",
+            data: {
+                "user":$("#userLogin").val(),
+                "senha":$("#senhaLogin").val()
+            }
+        });
+        request.done(function (response, textStatus, jqXHR){
+            console.log("Resposta do POST Login Recebida:");
+            console.log(response);
+            if(response.codigo === "200"){
+                user = $("#userLogin").val();
+                ocultarTudo();
+                $("#BotaoSair").show();
+                $("#SCANER").show();
+                tokenAcesso = response.token;
+            }else{
+                reportarErro(response.codigo);
+            }
+        });
+        request.fail(function (jqXHR, textStatus, errorThrown){
+            console.log("Erro ao receber a resposta do POST Login.");
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+            reportarErro("000");
+        });
+    });
+
+    var jaValidou = false;
+    function validarDadosLogin(){
+        var retorno = jaValidou = true;
+        if($("#senhaLogin").val() === ""){
+            $("#senhaLogin").removeClass("is-valid");
+            $("#senhaLogin").addClass("is-invalid");
+            $("#ERRO-SENHA").show();
+            retorno = false;
+        }else{
+            $("#ERRO-SENHA").hide();
+            $("#senhaLogin").removeClass("is-invalid");
+            $("#senhaLogin").addClass("is-valid");
+        }
+        if($("#userLogin").val() === ""){
+            $("#ERRO-USER").show();
+            $("#userLogin").removeClass("is-valid");
+            $("#userLogin").addClass("is-invalid");
+            retorno = false;
+        }else{
+            $("#ERRO-USER").hide();
+            $("#userLogin").removeClass("is-invalid");
+            $("#userLogin").addClass("is-valid");
+        }
+        return retorno;
+    }
+
+    $("#senhaLogin").keyup(function(){
+        if(jaValidou){
+            if($("#senhaLogin").val() != ''){
+                $("#ERRO-SENHA").hide();
+                $("#senhaLogin").removeClass("is-invalid");
+                $("#senhaLogin").addClass("is-valid");
+            }else{
+                $("#ERRO-SENHA").show();
+                $("#senhaLogin").removeClass("is-valid");
+                $("#senhaLogin").addClass("is-invalid");
+            }
+        }
+    });
+
+    $("#userLogin").keyup(function(){
+        if(jaValidou){
+            if($("#userLogin").val() != ''){
+                $("#ERRO-USER").hide();
+                $("#userLogin").removeClass("is-invalid");
+                $("#userLogin").addClass("is-valid");
+            }else{
+                $("#ERRO-USER").show();
+                $("#userLogin").removeClass("is-valid");
+                $("#userLogin").addClass("is-invalid");
+            }
+        }
+    });
+
+    $("#BotaoSair").on("click", function(event){
+        event.preventDefault();
+        ocultarTudo();
+        tokenAcesso = "";
+        user = "";
+        $("#LOGIN").show();
+    });
+
+    function ocultarTudo(){
+        $("#LOGIN").hide();
+        $("#CADASTRO").hide();
+        $("#SCANER").hide();
+        $("#senhaLogin").removeClass("is-valid").removeClass("is-invalid").val("");
+        $("#userLogin").removeClass("is-valid").removeClass("is-invalid").val("");
+        $("#BotaoSair").hide();
+        jaValidou = false;
+    }
+
+    function reportarErro(codigo){
+        $("#MODAL-MENSAGEM p").remove();
+        if(codigo === "000"){
+            $("#MODAL-MENSAGEM").append("<p class='text-justfy'>Ocorreu uma falha ao tentar realizar a comunicação com o servidor.</p>");
+        }else if(codigo === "400"){
+            $("#MODAL-MENSAGEM").append("<p class='text-justfy'>Erro ao processar a sua requisição. Tente novamente!</p>");
+        }else if(codigo === "401"){
+            $("#MODAL-MENSAGEM").append("<p class='text-justfy'>Usuário não autenticado, realize o login novamente!</p>");
+            tokenAcesso = "";
+        }else if(codigo === "401s"){
+            $("#MODAL-MENSAGEM").append("<p class='text-justfy'>Não foi possível realizar o login, senha incorreta!</p>");
+            tokenAcesso = "";
+        }else if(codigo === "404c"){
+            $("#MODAL-MENSAGEM").append("<p class='text-justfy'>Não foi possível realizar o login, usuário não encontrado!</p>");
+            tokenAcesso = "";
+        }else if(codigo === "404p"){
+            $("#MODAL-MENSAGEM").append("<p class='text-justfy'>Não foi possível localizar o produto scaneado.</p>");
+            tokenAcesso = "";
+        }else if(codigo === "500"){
+            $("#MODAL-MENSAGEM").append("<p class='text-justfy'>Ocorreu um erro interno no servidor ao processar a sua requisição. Tente novamente!</p>");
+        }else{
+            $("#MODAL-MENSAGEM").append("<p class='text-justfy'>Ocorreu um erro desconhecido! Realize o Login novamente!</p>");
+            tokenAcesso = "";
+        }
+        $("#BotaoAbrirModal").click();
+        ocultarTudo();
+        if(tokenAcesso != ""){
+            $("#SCANER").show();
+        }else{
+            $("#LOGIN").show();
+            $("#senhaLogin").val("");
+            $("#userLogin").val("");
+        }
+    }
 }
+
+function exibirInfoProduto(dados, desconto, valorComDesconto){
+    $("#MODAL-MENSAGEM p").remove();
+    mensagem  = "<p class='text-justfy'><b>Nome produto:</b> "+dados.nome+"</p>";
+    mensagem += "<p class='text-justfy'><b>Marca:</b> "+dados.marca+"</p>";
+    mensagem += "<p class='text-justfy'><b>Modelo:</b> "+dados.modelo+"</p>";
+    mensagem += "<p class='text-justfy'><b>Descrição:</b> "+dados.descricao+"</p>";
+    mensagem += "<p class='text-justfy'><b>Preço: </b> R$"+dados.preco.toFixed(2)+"</p>";
+    mensagem += "<p class='text-justfy'><b>Seu desconto: </b> "+desconto.toFixed(2)+"%</p>";
+    mensagem += "<p class='text-justfy'><b>Você vai pagar: </b> R$ "+valorComDesconto.toFixed(2)+"</p>";
+    $("#MODAL-MENSAGEM").append(mensagem);
+    $("#BotaoAbrirModal").click();
+}
+
+// ====== LEITOR QR-CODE =========
+
+function O(X) {
+    return document.getElementById(X);
+}
+
+var contador=0;
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+       // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        window.plugin.CanvasCamera.initialize(O('canvas'));
+        var options = {
+            cameraPosition: 'back',
+            width: 352,
+            height: 288,
+            canvas: {
+                width: 352,
+                height: 288
+            },
+            capture: {
+                width: 352,
+                height: 288
+            },
+            fps: 30,
+            use: 'file',
+            flashMode: false,
+            thumbnailRatio: 1/6,
+            onBeforeDraw: function(frame){
+            // do something before drawing a frame
+            },
+            onAfterDraw: function(frame){
+                var c = O('canvas');
+                if (contador == 10){
+                    const code = jsQR(((c.getContext("2d")).getImageData(0, 0, c.width, c.height)).data, c.width, c.height);
+                    if (code){
+                        window.plugin.CanvasCamera.stop();
+                        if (request) {
+                            request.abort();
+                        }
+                        request = $.ajax({
+                            type: "POST",
+                            url: HOST_IP+"/consulta",
+                            data: {
+                                "user":user,
+                                "token":tokenAcesso,
+                                "qrcode":code.data
+                            }
+                        });
+                        request.done(function (response, textStatus, jqXHR){
+                            console.log("Resposta do POST Consulta recebida:");
+                            console.log(response);
+                            if(response.codigo === "200"){
+                                exibirInfoProduto(response.dados, response.desconto, response.valorComDesconto);
+                            }else{
+                                reportarErro(response.codigo);
+                            }
+                        });
+                        request.fail(function (jqXHR, textStatus, errorThrown){
+                            console.log("Erro ao receber a resposta do POST Login.");
+                            console.log(jqXHR);
+                            console.log(textStatus);
+                            console.log(errorThrown);
+                            reportarErro("000");
+                        });
+                    }
+                    contador = 0;
+                }
+                else{
+                    contador++;
+                }         
+            }
+        };
+
+       O('BOTAO-SCAN').addEventListener('click',function(){
+            window.plugin.CanvasCamera.start(options);
+       }, false);
+    }
+};
+
+app.initialize();
--- trabalho-02/servidor/servidor.js
+++ trabalho-02/servidor/servidor.js
@@ -1,5 +1,14 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const app = express();
@@ -9,6 +18,9 @@ require('dotenv/config');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
+
+const server = https.createServer(options, app);
 
 const dbName = 'loja-departamentos';
 const client = new mongoClient(process.env.URL_MONGODB, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -23,55 +35,115 @@ client.connect(function(error) {
 
     console.log("Conectado ao banco de dados!");
 
-    app.listen(process.env.PORT_SERVER, "0.0.0.0", () => {
+    server.listen(process.env.PORT_SERVER, "0.0.0.0", () => {
         console.log("Servidor UP na porta " + process.env.PORT_SERVER + "!");
     });
 
     app.post("/login", function(req, resp){
+        console.log("POST Login");
+        console.log(req.body);
+        // verifica se a requisição está no formato correto.
         if(req.body.user && req.body.senha){
             const user = req.body.user;
             const senha = req.body.senha;
+            // realiza a busca do cliente no banco pelo user informado
+            db.collection('cliente').findOne({"user":user}, (erroConsultaCliente, cliente) => {
+                if(erroConsultaCliente){
+                    console.log("Erro ao realizar consulta no Banco de Dados. Erro 500");
+                    resp.send({"codigo":"500"}); // se ocorrer erro na busca retorna erro de servidor
+                    return;
+                }else{
+                    // se o cliente foi encontrado realiza a comparação da senha
+                    if(cliente){
+                        bcrypt.compare(senha, cliente.senha, function(err, result) {
+                            // se resut for true, realiza login. Se não retorna erro de altenticação
+                            if(result){
+                                // gera um token de acesso e envia para o cliente armazenar.
+                                bcrypt.genSalt(saltRounds, function(err, salt) {
+                                    bcrypt.hash("Hash", salt, function(err, token) {
+                                        bcrypt.genSalt(saltRounds, function(err, salt) {
+                                            bcrypt.hash(token, salt, function(err, hashToken) {
+                                                // armazena o hash do token no banco de dados e envia o token para o cliente
+                                                db.collection('cliente').updateOne({"user":user}, {$set: {'token':hashToken}});
+                                                console.log("Cliente Autorizado. Retorno 200");
+                                                resp.send({"codigo":"200", "token":token});
+                                            });
+                                        });
+                                    });
+                                });
+                            }else{
+                                console.log("Cliente não autorizado. Erro 401s (senha incorreta)");
+                                resp.send({"codigo":"401s"});
+                            }
+                        });
+                    }else{
+                        console.log("Consulta realizada mas usuário não encontrado. Erro 404");
+                        resp.send({"codigo":"404c"});
+                    }
+                }
+            });
         }else{
-            // retorna erro entrada
+            console.log("Requisição incorreta, erro 400");
+            resp.send({"codigo":"400"});
         }
     });
 
     app.post("/cadastro", function(req, resp){
-        if(req.body.user && req.body.senha){
-
+        if(req.body.user && req.body.senha && req.body.nome){
+            // realizar veriricação dos dados recebidos e insert no banco
         }else{
             // erro requisição
         }
     })
 
     app.post("/consulta", function(req, resp){
-        if(req.body.iduser && req.body.qrcode){
-            db.collection('produto').findOne({"_id":ObjectID("5fd8639428022a18ac8cd944")},  (erroConsultaProduto, produto) => {
-                if(erroConsultaProduto){
-                    console.log("Erro Consulta Produto");
+        console.log("POST Consulta");
+        console.log(req.body);
+        if(req.body.user && req.body.qrcode && req.body.token){
+            const user = req.body.user;
+            const token = req.body.token;
+            const qrcode = req.body.qrcode;
+            db.collection('cliente').findOne({"user":user},  (erroConsultaCliente, cliente) => {
+                if(erroConsultaCliente){
+                    resp.send({"codigo":"500"}); // erro interno de servidor
+                    return;
                 }else{
-                    db.collection('cliente').findOne({"_id":ObjectID("5fd85dd5d122514728cb9aa9")}, (erroConsultaCliente, cliente) => {
-                        if(erroConsultaCliente){
-                            console.log("Erro Consulta Cliente");
-                        }else{
-                            console.log(produto);
-                            console.log(cliente);
-                        }
-                    });
+                    if(cliente){
+                        bcrypt.compare(token, cliente.token, function(err, result) {
+                            if(result){
+                                db.collection('produto').findOne({"_id":ObjectID(qrcode)}, (erroConsultaProduto, produto) => {
+                                    if(erroConsultaProduto){
+                                        resp.send({"codigo":"500"}); // erro interno de servidor
+                                        return;
+                                    }else{
+                                        if(produto){ // se o cliente está autorizado e os dados do produto foram encontrados
+                                            var desconto = parseFloat(cliente.desconto)*100.0;
+                                            var valorComDesconto = parseFloat(produto.preco) - parseFloat(produto.preco)*parseFloat(cliente.desconto);
+                                            console.log(desconto);
+                                            console.log(valorComDesconto);
+                                            resp.send({"codigo":"200", "dados":produto, "desconto":desconto, "valorComDesconto":valorComDesconto});
+                                        }else{ // erro produto não encontrado
+                                            resp.send({"codigo":"404p"}); // erro produto não encontrado
+                                            return;
+                                        }
+                                        
+                                    }
+                                });
+                            }else{
+                                resp.send({"codigo":"401"}); // erro cliente não autorizado
+                                return;
+                            }
+                        })
+                    }else{
+                        resp.send({"codigo":"404c"}); // erro cliente não encontrado
+                        return;
+                    }
+                    
                 }
             });
         }else{
-            resp.send();
+            resp.send({"codigo":"400"}); // erro POST em formato incorreto
+            return;
         }
     });
 });
-
-bcrypt.genSalt(saltRounds, function(err, salt) {
-    bcrypt.hash("040404", salt, function(err, hash) {
-        console.log(hash);
-    });
-});
-// $2b$10$yMxEUcfc7iHZA6SUAagktOuOrAq.soLnc7IEKOdyyJfRJsTwbZLPq
-//bcrypt.compare("000001", "$2b$10$yMxEUcfc7iHZA6SUAagktOuOrAq.soLnc7IEKOdyyJfRJsTwbZLPq", function(err, result) {
-//    console.log(result);
-//});
